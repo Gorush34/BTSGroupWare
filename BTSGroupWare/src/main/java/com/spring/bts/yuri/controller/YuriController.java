3867711212518367c57ebd3c687b364b92a85198
yuri <sist@sist>
2022-04-27 21:29:22 +0900

    jsp, controller commit

--- BTSGroupWare/src/main/java/com/spring/bts/yuri/controller/YuriController.java
+++ BTSGroupWare/src/main/java/com/spring/bts/yuri/controller/YuriController.java
@@ -20,57 +20,68 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller /* Bean + controller 기능을 모듀 포함 */
 public class YuriController {
 
-		
-	/* ***** bts 그룹웨어 파이널 프로젝트 용 컨트롤러 시작 **************************************************************/
 	
-	// === bts 전자결재 홈 페이지 === //
-	@RequestMapping(value="/test/edmsHome.bts")
+	// === 전자결재 테스트 페이지 === //
+	@RequestMapping(value="/edms/edmsTest.bts")
+	public ModelAndView edmsTest(HttpServletRequest request, HttpServletResponse response, ModelAndView mav) {
+
+		mav.setViewName("edms_test.edms");
+		// /WEB-INF/views/edms/{1}.jsp
+		// /WEB-INF/views/edms/edms_test.jsp 페이지를 만들어야 한다.
+		return mav;
+		
+	}
+	
+	// === 전자결재 홈 페이지 === //
+	@RequestMapping(value="/edms/edmsHome.bts")
 	public ModelAndView edmsHome(HttpServletRequest request, HttpServletResponse response, ModelAndView mav) {
 
-		mav.setViewName("edmsHome.tiles3");
-		// /WEB-INF/views/tiles3/{1}.jsp
-		// /WEB-INF/views/tiles3/edmsHome.jsp 페이지를 만들어야 한다.
+		mav.setViewName("edmsHome.edms");
+		// /WEB-INF/views/edms/{1}.jsp
+		// /WEB-INF/views/edms/edmsHome.jsp 페이지를 만들어야 한다.
 		return mav;
 		
 	}
 
 
-	// === bts 게시판 글쓰기 폼페이지 요청 === //
-	@RequestMapping(value="/test/edmsAdd.bts", produces="text/plain;charset=UTF-8")
+	// === 전자결재 문서작성 페이지 === //
+	@RequestMapping(value="/edms/edmsAdd.bts", produces="text/plain;charset=UTF-8")
 	public ModelAndView addEdms(HttpServletRequest request, HttpServletResponse response, ModelAndView mav) {
 		
 	//	getCurrentURL(request); // 로그아웃을 했을 때  현재 보이던 그 페이지로 그대로 돌아가기 위한 메소드 호출
 		// 위의 문장을 주석처리하고 게시판 - 글쓰기 - 로그인 - 로그아웃 을 하면  goBackURL이 없으므로 시작페이지로 간다!
 		
-		mav.setViewName("edmsAdd.tiles3");
-		//	/WEB-INF/views/tiles3/edmsAdd.jsp 파일을 생성한다
-		
+		mav.setViewName("edmsAdd.edms");
+		// /WEB-INF/views/edms/{1}.jsp
+		// /WEB-INF/views/edms/edmsAdd.jsp 페이지를 만들어야 한다.
 		return mav;
 	}
 	
 	
 	// === bts 내문서함 페이지 === //
-	@RequestMapping(value="/test/edmsMydoc.bts", produces="text/plain;charset=UTF-8")
+	@RequestMapping(value="/edms/edmsMydoc.bts", produces="text/plain;charset=UTF-8")
 	public ModelAndView edmsMydoc(HttpServletRequest request, HttpServletResponse response, ModelAndView mav) {
 		
 	//	getCurrentURL(request); // 로그아웃을 했을 때  현재 보이던 그 페이지로 그대로 돌아가기 위한 메소드 호출
 		// 위의 문장을 주석처리하고 게시판 - 글쓰기 - 로그인 - 로그아웃 을 하면  goBackURL이 없으므로 시작페이지로 간다!
 		
-		mav.setViewName("edmsMydoc.tiles3");
-		//	/WEB-INF/views/tiles3/edmsMydoc.jsp 파일을 생성한다
-		
+		mav.setViewName("edmsMydoc.edms");
+		// /WEB-INF/views/edms/{1}.jsp
+		// /WEB-INF/views/edms/edmsMydoc.jsp 페이지를 만들어야 한다.
 		return mav;
 	}
 	
+	
 	// === bts 결재하기 페이지 === //
-	@RequestMapping(value="/test/edmsApprove.bts", produces="text/plain;charset=UTF-8")
+	@RequestMapping(value="/edms/edmsApprove.bts", produces="text/plain;charset=UTF-8")
 	public ModelAndView edmsApprove(HttpServletRequest request, HttpServletResponse response, ModelAndView mav) {
 		
 	//	getCurrentURL(request); // 로그아웃을 했을 때  현재 보이던 그 페이지로 그대로 돌아가기 위한 메소드 호출
 		// 위의 문장을 주석처리하고 게시판 - 글쓰기 - 로그인 - 로그아웃 을 하면  goBackURL이 없으므로 시작페이지로 간다!
 		
-		mav.setViewName("edmsApprove.tiles3");
-		//	/WEB-INF/views/tiles3/edmsApprove.jsp 파일을 생성한다
+		mav.setViewName("edmsApprove.edms");
+		// /WEB-INF/views/edms/{1}.jsp
+		// /WEB-INF/views/edms/edmsApprove.jsp 페이지를 만들어야 한다.
 		return mav;
 	}
 	
