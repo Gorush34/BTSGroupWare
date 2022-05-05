4b64ace844d5ff124e598c6762b0e0484f836c30
yuri <sist@sist>
2022-05-05 12:26:36 +0900

    중간 commit

--- BTSGroupWare/src/main/java/com/spring/bts/hwanmo/controller/TestController.java
+++
@@ -1,5 +0,0 @@
-package com.spring.bts.hwanmo.controller;
-
-public class TestController {
-	// 개인브랜치도 되나요?
-}
---
+++ BTSGroupWare/src/main/java/com/spring/bts/hwanmo/model/AttendanceVO.java
@@ -0,0 +1,5 @@
+package com.spring.bts.hwanmo.model;
+
+public class AttendanceVO {
+
+}
